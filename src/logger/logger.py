1203f769db3b80350ffe2a5e94ee1250877178de
Пардаев Азизбек Аътамжон-угли <aapardaev_1@miem.hse.ru>
2023-01-23 17:31:16 +0500

    📝 change logging level to debug

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -1,12 +1,12 @@
 import logging
 
 log = logging.getLogger()
-log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
 
 full_log = logging.FileHandler("logs/full.log", "a", "utf-8")
-full_log.setLevel(logging.INFO)
+full_log.setLevel(logging.DEBUG)
 full_log.setFormatter(formatter)
 log.addHandler(full_log)
 
